f46f6722bcec5f744b5ddb2263bece8f2dbdd53a
刘备 <admin@yitianyigexiangfa.com>
2021-06-15 17:48:56 +0800

    【每个请求一个线程】千万不要这么做

---
+++ java_concurrency_in_practice/src/main/java/liubei/juc/socket_thread/ThreadPerTaskWebServer.java
@@ -0,0 +1,29 @@
+package liubei.juc.socket_thread;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ThreadPerTaskWebServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(8080);
+        while (true) {
+            final Socket connection = socket.accept();
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    handleRequest(connection);
+                }
+            };
+
+            new Thread(runnable).start();
+        }
+    }
+
+    private static void handleRequest(Socket socket) {
+        System.out.println("thread Name: " + Thread.currentThread().getName()
+                + socket.getInetAddress().getAddress());
+    }
+
+}
